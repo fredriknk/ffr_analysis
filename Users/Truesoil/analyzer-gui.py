94e0b0b27e38f75659ffa5301578fe855c8355b4
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2024-03-05 10:29:44 +0100

    Truesoil push

--- Users/Truesoil/analyzer-gui.py
+++ Users/Truesoil/analyzer-gui.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logging.getLogger('matplotlib.font_manager').disabled = True
 
-    project_name = 'Greenmove'
+    project_name = 'Truesoil'
 
     flux_units = {'N2O': {'name': 'N2O_N_mug_m2h', 'factor': 2 * 14 * 1e6 * 3600},
                   'CO2': {'name': 'CO2_C_mug_m2h', 'factor': 12 * 1e6 * 3600}}
@@ -36,19 +36,10 @@ if __name__ == "__main__":
         }
     }
     treatment_legend = {
-        1: {"name": "Barley+ryegrass Moved", "treatment": "2B", "plots": (1, 6, 7, 12)},
-        2: {"name": "Barley+ryegrass Control", "treatment": "2A", "plots": (2, 5, 8, 11)},
-        3: {"name": "Barley+ryegrass Mov+Min", "treatment": "2C", "plots": (3, 4, 9, 10)},
-        4: {"name": "Barley Moved", "treatment": "1B", "plots": (13, 18, 19, 24)},
-        5: {"name": "Barley Control", "treatment": "1A", "plots": (14, 17, 20, 23)},
-        6: {"name": "Barley Mov+Min", "treatment": "1C", "plots": (15, 16, 21, 22)},
-        7: {"name": "Cover crops Moved", "treatment": "3B", "plots": (25, 30, 31, 36)},
-        8: {"name": "Cover crops Control", "treatment": "3A", "plots": (26, 29, 32, 35)},
-        9: {"name": "Cover crops Mov+Min", "treatment": "3C", "plots": (27, 28, 33, 34)},
-        10: {"name": "Ley Moved", "treatment": "4B", "plots": (37, 42, 43, 48)},
-        11: {"name": "Ley Control", "treatment": "4A", "plots": (38, 41, 44, 47)},
-        12: {"name": "Ley Mov+Min", "treatment": "4C", "plots": (39, 40, 45, 46)}
-
+        1: {"name": "Regntak", "treatment": "R", "plots": (1, 3, 5, 7, 9, 11, 13, 15)},
+        2: {"name": "Kontroll (regntak)", "treatment": "K", "plots": (2, 4, 6, 8, 10, 12, 14, 16)},
+        3: {"name": "Kontroll (overfor)", "treatment": "O",
+            "plots": (17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32)}
     }
 
     persistent_column_selection = ['date', 'CO2_C_mug_m2h', 'N2O_N_mug_m2h', "type",
